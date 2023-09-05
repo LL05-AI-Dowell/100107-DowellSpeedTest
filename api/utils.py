340eb12749d95c703acc1fc35d61976140d21d01
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2023-09-06 00:59:43 +0100

    Initial commit. Class for fetching website info implemented in api.utils

---
+++ api/utils.py
@@ -0,0 +1,263 @@
+from bs4_web_scraper.scraper import BS4WebScraper
+from urllib3.util import parse_url
+from urllib.parse import unquote_plus
+from difflib import get_close_matches
+from concurrent.futures import ThreadPoolExecutor
+
+scraper = BS4WebScraper()
+
+SOCIAL_PLATFORMS = {
+    "facebook": "https://www.facebook.com/",
+    "twitter": "https://twitter.com/",
+    "instagram": "https://www.instagram.com/",
+    "linkedin": "https://www.linkedin.com/company/",
+    "youtube": "https://www.youtube.com/",
+    "pinterest": "https://www.pinterest.com/",
+    "tumblr": "https://www.tumblr.com/",
+    "reddit": "https://www.reddit.com/user/",
+    "flickr": "https://www.flickr.com/people/",
+    "snapchat": "https://www.snapchat.com/add/",
+    "whatsapp": "https://wa.me/",
+    "telegram": "https://t.me/",
+    "wechat": "https://weixin.qq.com/",
+    "line": "https://line.me/R/ti/p/",
+    "viber": "https://chats.viber.com/",
+    "vk": "https://vk.com/",
+    "tiktok": "https://www.tiktok.com/@",
+    "soundcloud": "https://soundcloud.com/",
+    "spotify": "https://open.spotify.com/user/",
+    "medium": "https://medium.com/@",
+    "quora": "https://www.quora.com/profile/",
+    "twitch": "https://www.twitch.tv/",
+    "behance": "https://www.behance.net/",
+    "dribbble": "https://dribbble.com/",
+    "deviantart": "https://www.deviantart.com/",
+    "foursquare": "https://foursquare.com/",
+    "goodreads": "https://www.goodreads.com/",
+    "hackernews": "https://news.ycombinator.com/user?id=",
+    "producthunt": "https://www.producthunt.com/@",
+    "tripadvisor": "https://www.tripadvisor.com/members/",
+    "yelp": "https://www.yelp.com/user_details?userid=",
+    "wordpress": "https://profiles.wordpress.org/",
+    "blogger": "https://www.blogger.com/profile/",
+    "wix": "https://www.wix.com/dashboard/",
+    "weebly": "https://www.weebly.com/editor/main.php#/site/",
+    "jimdo": "https://www.jimdo.com/app/profile/",
+    "squarespace": "https://www.squarespace.com/preview/",
+}
+
+class WebsiteInfoFinder:
+
+    def __init__(self, web_url: str, engine: BS4WebScraper = scraper, max_search_depth: int = 0) -> None:
+        """
+        Initializes a WebsiteInfoFinder object.
+
+        :param web_url: The url of the website or webpage to find information about.
+        :param engine: The web scraper to use to find information about the website. \
+        Should be of type BS4WebScraper.
+        :param max_search_depth: The maximum depth to search for information on the website. \
+        The higher the depth, the longer it takes to find information. \
+        Defaults to 0, which means only the base url is searched.\
+        If set to 1, the base url and all the links on the base url are searched. \
+        If set to 2, the base url, all the links on the base url and all the links on the links on the base url are searched and so on
+        """
+        if not isinstance(engine, BS4WebScraper):
+            raise TypeError(f"Expected scraper to be of type {BS4WebScraper.__name__}, got {type(scraper)} instead.")
+        self.target = web_url
+        self.engine = engine
+        self.maximum_search_depth = max_search_depth
+
+    def find_website_name(self):
+        base_url = self.engine.get_base_url(self.target)
+        set_1 = self.engine.find_all_tags(
+            url=base_url,
+            target="meta",
+            attrs={
+                "name": ["application-name", "apple-mobile-web-app-title"],
+            },
+            depth=self.maximum_search_depth
+        )
+        set_2 = self.engine.find_all_tags(
+            url=base_url,
+            target="meta",
+            attrs={
+                "property": ["og:site_name", "og:title"],
+            },
+            depth=self.maximum_search_depth
+        )
+        set_3 = self.engine.find_all_tags(
+            url=base_url,
+            target="meta",
+            attrs={
+                "itemprop": ["name"],
+            },
+            depth=self.maximum_search_depth
+        )
+        set_4 = self.engine.find_all_tags(
+            url=base_url,
+            target="title",
+            depth=self.maximum_search_depth
+        )
+        tags = set_1 + set_2 + set_3 + set_4
+        results = []
+        for tag in tags:
+            content = tag.get("content", None) or tag.text
+            if content:
+                results.append(content)
+        names = set(results)
+        host = parse_url(base_url).host
+        matches = get_close_matches(host, names, n=len(names)//2 or 1, cutoff=0.3)
+        return matches[0] if matches else None
+    
+
+    def find_emails(self):
+        """
+        Finds all the emails on the website.
+
+        :return: A list of emails found.
+        """
+        result = self.engine.find_emails(url=self.target, depth=self.maximum_search_depth)
+        return list(set(result))
+
+    
+
+    def find_phone_numbers(self):
+        """
+        Finds all the phone numbers on the website.
+
+        :return: A list of phone numbers found.
+        """
+        result = self.engine.find_phone_numbers(url=self.target, depth=self.maximum_search_depth)
+        return list(set(result))
+        
+
+    def find_links(self):
+        """
+        Finds all the links on the website.
+
+        :return: A list of urls of the links found.
+        """
+        return self.engine.find_links(url=self.target, depth=self.maximum_search_depth)
+
+
+    def find_logos(self):
+        """
+        Finds the logos of the website. 
+        
+        Checks the Favicons, Apple Touch Icons because they are more likely to be the correct ones.
+        :return: A list of urls of the logos found.
+        """
+        logo_tags = self.engine.find_all_tags(
+            url=self.target, 
+            target="link",
+            attrs={
+                "rel": ["icon", "shortcut icon", "apple-touch-icon", "apple-touch-icon-precomposed"]
+            },
+            depth=self.maximum_search_depth
+        )
+        # Get image urls from meta tags on the websites home page
+        image_meta_tags = self.engine.find_all_tags(
+            url=self.engine.get_base_url(self.target), 
+            target="meta",
+            attrs={
+                "property": ["og:image", "twitter:image", "twitter:image:src"]
+            },
+            depth=self.maximum_search_depth
+        )
+        logo_tags.extend(image_meta_tags)
+        logos = [ self.engine.get_tag_rra(tag, download=False) for tag in logo_tags ]
+        return list(filter(lambda x : bool(x), logos))
+    
+
+    def guess_page_url(self, page_name: str):
+        """
+        Tries to guess the correct url of a page on the website based on the page name.
+
+        :param page_name: The name of the page to guess the url of. \
+            Can be the name of an external page or a page on the website\
+                  as long as it can related to a url on the website.
+        :return: url of the page if found, None otherwise.
+        """
+        base_url = self.engine.get_base_url(self.target)
+        links = self.engine.find_links(
+            url=base_url,
+            depth=self.maximum_search_depth
+        )
+        # Try and find a close match
+        urls = {unquote_plus(link) : link for link in links}
+        matches = get_close_matches(page_name, urls.keys(), n=len(urls)//2 or 1, cutoff=0.3)
+        match = urls.get(matches[0]) if matches else None
+        if match:
+            return match
+        # If no close match, try and find a link with the page name in it
+        for link in links:
+            if page_name.lower() in link.lower() or page_name.strip().lower() in link.lower():
+                return link
+        # If still no match, try and find a link with the page name in its text
+        link_tags = self.engine.find_all_tags(
+            url=base_url,
+            target='a',
+            depth=self.maximum_search_depth
+        )
+        for tag in link_tags:
+            if page_name.lower() in tag.text.lower():
+                return tag.get("href", None)
+        return None
+    
+
+    def find_website_social_handle_for(self, platform_name: str):
+        """
+        Finds social media handle for the website on a social media platform.
+
+        This checks the contact and about pages first for social handle,
+        because the one on the website details page is more likely to be the correct one.
+        If the social handle is not found on those pages, it checks the base url for any link related to the social media platform.
+
+        :param platform_name: The name of the social media platform to find the social handle for.
+        :return: The social handle if found, None otherwise.
+        """
+        # Check contact and about pages first for social handle, 
+        # because the one on the website details page is more likely to be the correct one.
+        for page_name in ("contact", "about"):
+            page_url = self.guess_page_url(page_name)
+            if page_url and parse_url(page_url).scheme in ["http", "https"]:
+                new_finder = self.__class__(page_url, self.engine, self.maximum_search_depth)
+                related_links = new_finder.find_links_related_to(platform_name)
+                if related_links:
+                    website_name = self.find_website_name()
+                    social_handle_matches = get_close_matches(website_name, related_links, n=len(related_links)//2 or 1, cutoff=0.3)
+                    if social_handle_matches:
+                        return social_handle_matches[0]
+            continue
+        
+        # If url cannot be found in those pages, resort to checking the base url for social handle.
+        # This is less likely to be the correct one, but it's better than nothing.
+        social_url = self.guess_page_url(platform_name)
+        return social_url
+    
+
+    def find_links_related_to(self, _s: str):
+        """
+        Similar to find_links, but only returns links that contain the string _s.
+
+        :param _s: The string to search for in the links.
+        :return: A list of urls of the links found.
+        """
+        links = self.find_links()
+        matches = [ link for link in links if _s.lower() in link.lower() ]
+        return matches
+
+
+    def find_all_social_media_links(self):
+        result = {}
+        with ThreadPoolExecutor() as executor:
+            executor.map(
+                lambda platform_name: setattr(result, platform_name, self.find_links_related_to(platform_name)), 
+                SOCIAL_PLATFORMS.keys()
+            )
+        return list(filter(lambda x: bool(x), result))
+
+
+if __name__ == "__main__":
+    finder = WebsiteInfoFinder(web_url="https://dribbble.com/", max_search_depth=0)
+    print(finder.find_emails())
---
+++ api/views.py
@@ -0,0 +1,6 @@
+from rest_framework import generics
+
+
+
+class WebsiteInfoAPIView(generics.GenericAPIView):
+    pass
