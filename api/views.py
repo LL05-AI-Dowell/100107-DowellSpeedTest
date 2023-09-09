7403179723dc9b3f9c7ec9f774670cfc4f60a7ed
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2023-09-09 20:05:27 +0100

    Bug fixes

--- api/views.py
+++ api/views.py
@@ -13,8 +13,10 @@ class WebsiteInfoExtractionAPIView(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             web_info_request = WebsiteInfoRequest(body=serializer.validated_data)
-            response = web_info_request.get_json_response()
-            return response
+            response_dict = web_info_request.get_response_dict()
+            if response_dict:
+                return Response(response_dict, status=status.HTTP_200_OK)
+            serializer.errors.update(web_info_request.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
