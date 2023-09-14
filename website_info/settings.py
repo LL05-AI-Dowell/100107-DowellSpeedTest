6db79769cf166e77ef803ece0cedcb4ae0bea26c
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2023-09-14 04:58:19 +0100

    Removed djsm from settings.py

--- website_info/settings.py
+++ website_info/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from djsm import djsm
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = djsm.get_or_create_secret_key()
+SECRET_KEY = "wsredtrfkgtyuhoijpkhgjbwqgqrelgmheh;meh;lhqhq;lkqeh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
