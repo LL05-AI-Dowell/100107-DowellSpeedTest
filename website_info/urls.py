e88811cddb3e2a85c0663b3b8a595820ee5483dd
Manish <mdashsharma95@gmail.com>
2023-10-01 05:24:59 +0530

    added health checker endpoints

--- website_info/urls.py
+++ website_info/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from healthcheck import *
 
 urlpatterns = [
+    path('', health_check.as_view()),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'), name="api"),
     path('api/v1/', include('APP.urls')),
